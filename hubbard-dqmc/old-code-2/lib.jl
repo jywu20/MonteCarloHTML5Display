bac48d03413eb6d187a5369f1d6932118f198c1f
wujinq <wujinyuan111@126.com>
2022-05-30 19:03:16 +0800

    deleting unncessary codes

--- hubbard-dqmc/old-code-2/lib.jl
+++
@@ -1,9 +0,0 @@
-include("utils.jl")
-include("lattice.jl")
-
-using LinearAlgebra
-using Random
-using Statistics
-
-include("hubbard-init.jl")
-include("hubbard-sweep.jl")
--- ising-data-collapsing/utils.js
+++
@@ -1,3 +0,0 @@
-function sum(arr) {
-    return arr.reduce((a, b) => a + b);
-}
